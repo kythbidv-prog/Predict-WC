650a0034bc4209e40ea3fe3ba4b635f28d8db172
kythbidv-prog <kythbidv@gmail.com>
2026-06-28 19:04:40 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Dán Firebase Web App config thật của bạn vào đây.
 // Project settings > Your apps > Web app > firebaseConfig.
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAG_WRfmW0kWZivaPCvbqEFwmvFHMs55ZQ",
+  authDomain: "worldcup2026-168f1.firebaseapp.com",
+  projectId: "worldcup2026-168f1",
+  storageBucket: "worldcup2026-168f1.firebasestorage.app",
+  messagingSenderId: "750459892029",
+  appId: "1:750459892029:web:c6d3ddc65a41101a5a5554"
 };
